9312178eb70bb2b7303629d4491638a0201973e1
hegotgame <jgill@uwm.edu>
2017-07-25 22:42:05 -0500

    Filling default windows.prompt with dropdown box values

--- app.js
+++ app.js
@@ -91,3 +91,63 @@ function yesNo(input){
 function chars(input){
   return true; // default validation only
 }
+function getInputs () {
+  var put1 = document.getElementById("name");
+    var z1 = put1.options[put1.selectedIndex].value;
+  var put2 = document.getElementById("lastName");
+    var z2 = put2.options[put2.selectedIndex].value;
+  var put3 = document.getElementById("firstName");
+    var z3 = put3.options[put3.selectedIndex].value;
+  var put4 = document.getElementById("eyeColor");
+    var z4 = put4.options[put4.selectedIndex].value;
+  var put5 = document.getElementById("gender");
+    var z5 = put5.options[put5.selectedIndex].value;
+  var put6 = document.getElementById("occupation");
+    var z6 = put6.options[put6.selectedIndex].value;
+  var arrZ = [z1, z2, z3, z4, z5, z6];
+  console.log(arrZ);
+  return arrZ;
+}
+
+
+function appName(people){
+var choice = getInputs();
+
+  var searchType = window.prompt("Do you know the name of the person you are looking for? Please enter 'yes' or 'no'", choice[0]);
+  switch(searchType){
+    case 'yes':
+    // TODO: function integration
+    break;
+    case 'no':
+    // TODO: search by traits
+    break;
+    default:
+    app(people); // restart app
+    break;
+  }
+  }
+function appLastName(people){
+var choice = getInputs();
+
+  var searchType = window.prompt("Do you know the last name of the person you are looking for?", choice[1]);
+  }
+function appFirstName(people){
+var choice = getInputs();
+
+  var searchType = window.prompt("Do you know the first name of the person you are looking for?", choice[2]);
+}
+function appEyeColor(people){
+var choice = getInputs();
+
+  var searchType = window.prompt("Do you know the eye color of the person you are looking for?", choice[3]);
+}
+function appGender(people){
+var choice = getInputs();
+
+  var searchType = window.prompt("Do you know the gender of the person you are looking for?", choice[4]);
+}
+function appOccupation(people){
+var choice = getInputs();
+
+  var searchType = window.prompt("Do you know the occupation of the person you are looking for?", choice[5]);
+}
